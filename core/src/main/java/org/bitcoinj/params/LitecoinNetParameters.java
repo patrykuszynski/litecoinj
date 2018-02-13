1a4289358b74293899302e9ec84e5cca70862b8e
Danila Samosudov <samosudovd@gmail.com>
2018-02-13 17:56:01 +0300

    ltc: LitecoinNetParameters - revert deprecated params

--- core/src/main/java/org/bitcoinj/params/LitecoinNetParameters.java
+++ core/src/main/java/org/bitcoinj/params/LitecoinNetParameters.java
@@ -16,8 +16,8 @@ public class LitecoinNetParameters extends NetworkParameters{
 
     public LitecoinNetParameters() {
         super();
-        fixTimeWarpBug = true;
-        dustSoftLimit = Coin.MILLICOIN;
+//        fixTimeWarpBug = true;
+//        dustSoftLimit = Coin.MILLICOIN;
         alertSigningKey = Utils.HEX.decode("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9");
 //        blockVerifyFunction = new LitecoinVerifyFunction();
         targetTimespan = TARGET_TIMESPAN / 4;

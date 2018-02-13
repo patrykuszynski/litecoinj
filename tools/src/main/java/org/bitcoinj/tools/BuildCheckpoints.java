b0bf61415b48fb6c006c01c61430cb7b11243aa5
Danila Samosudov <samosudovd@gmail.com>
2018-02-13 18:52:39 +0300

    BuildCheckpoints from bitcoinj

--- tools/src/main/java/org/bitcoinj/tools/BuildCheckpoints.java
+++ tools/src/main/java/org/bitcoinj/tools/BuildCheckpoints.java
@@ -15,17 +15,23 @@
  * limitations under the License.
  */
 
-package org.litecoinj.integrations;
+package org.bitcoinj.tools;
 
+import org.bitcoinj.core.listeners.NewBestBlockListener;
 import org.bitcoinj.core.*;
-import org.bitcoinj.core.listeners.AbstractBlockChainListener;
+import org.bitcoinj.net.discovery.DnsDiscovery;
 import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.params.RegTestParams;
+import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.store.BlockStore;
 import org.bitcoinj.store.MemoryBlockStore;
 import org.bitcoinj.utils.BriefLogFormatter;
 import org.bitcoinj.utils.Threading;
 import com.google.common.base.Charsets;
-import org.litecoinj.integrations.LitecoinNetParameters;
+import com.google.common.io.Resources;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
 
 import java.io.DataOutputStream;
 import java.io.File;
@@ -35,75 +41,139 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.security.DigestOutputStream;
 import java.security.MessageDigest;
-import java.util.Date;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.concurrent.Future;
 
 import static com.google.common.base.Preconditions.checkState;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * Downloads and verifies a full chain from your local peer, emitting checkpoints at each difficulty transition period
  * to a file which is then signed with your key.
  */
 public class BuildCheckpoints {
-
-    private static final NetworkParameters PARAMS = LitecoinNetParameters.get();
-    private static final File PLAIN_CHECKPOINTS_FILE = new File("/tmp/checkpoints");
-    private static final File TEXTUAL_CHECKPOINTS_FILE = new File("/tmp/checkpoints.txt");
+    private static NetworkParameters params;
 
     public static void main(String[] args) throws Exception {
-        BriefLogFormatter.init();
+        BriefLogFormatter.initWithSilentBitcoinJ();
+
+        OptionParser parser = new OptionParser();
+        parser.accepts("help");
+        OptionSpec<NetworkEnum> netFlag = parser.accepts("net").withRequiredArg().ofType(NetworkEnum.class).defaultsTo(NetworkEnum.MAIN);
+        parser.accepts("peer").withRequiredArg();
+        OptionSpec<Integer> daysFlag = parser.accepts("days").withRequiredArg().ofType(Integer.class).defaultsTo(30);
+        OptionSet options = parser.parse(args);
+
+        if (options.has("help")) {
+            System.out.println(Resources.toString(BuildCheckpoints.class.getResource("build-checkpoints-help.txt"), Charsets.UTF_8));
+            return;
+        }
+
+        final String suffix;
+        switch (netFlag.value(options)) {
+            case MAIN:
+            case PROD:
+                params = MainNetParams.get();
+                suffix = "";
+                break;
+            case TEST:
+                params = TestNet3Params.get();
+                suffix = "-testnet";
+                break;
+            case REGTEST:
+                params = RegTestParams.get();
+                suffix = "-regtest";
+                break;
+            default:
+                throw new RuntimeException("Unreachable.");
+        }
+
+        // Configure bitcoinj to fetch only headers, not save them to disk, connect to a local fully synced/validated
+        // node and to save block headers that are on interval boundaries, as long as they are <1 month old.
+        final BlockStore store = new MemoryBlockStore(params);
+        final BlockChain chain = new BlockChain(params, store);
+        final PeerGroup peerGroup = new PeerGroup(params, chain);
+
+        final InetAddress ipAddress;
+
+        // DNS discovery can be used for some networks
+        boolean networkHasDnsSeeds = params.getDnsSeeds() != null;
+        if (options.has("peer")) {
+            // use peer provided in argument
+            String peerFlag = (String) options.valueOf("peer");
+            try {
+                ipAddress = InetAddress.getByName(peerFlag);
+                startPeerGroup(peerGroup, ipAddress);
+            } catch (UnknownHostException e) {
+                System.err.println("Could not understand peer domain name/IP address: " + peerFlag + ": " + e.getMessage());
+                System.exit(1);
+                return;
+            }
+        } else if (networkHasDnsSeeds) {
+            // for PROD and TEST use a peer group discovered with dns
+            peerGroup.setUserAgent("PeerMonitor", "1.0");
+            peerGroup.setMaxConnections(20);
+            peerGroup.addPeerDiscovery(new DnsDiscovery(params));
+            peerGroup.start();
+
+            // Connect to at least 4 peers because some may not support download
+            Future<List<Peer>> future = peerGroup.waitForPeers(4);
+            System.out.println("Connecting to " + params.getId() + ", timeout 20 seconds...");
+            // throw timeout exception if we can't get peers
+            future.get(20, SECONDS);
+        } else {
+            // try localhost
+            ipAddress = InetAddress.getLocalHost();
+            startPeerGroup(peerGroup, ipAddress);
+        }
 
         // Sorted map of block height to StoredBlock object.
         final TreeMap<Integer, StoredBlock> checkpoints = new TreeMap<Integer, StoredBlock>();
 
-        // Configure bitcoinj to fetch only headers, not save them to disk, connect to a local fully synced/validated
-        // node and to save block headers that are on interval boundaries, as long as they are <1 month old.
-        final BlockStore store = new MemoryBlockStore(PARAMS);
-        final BlockChain chain = new BlockChain(PARAMS, store);
-        final PeerGroup peerGroup = new PeerGroup(PARAMS, chain);
-        peerGroup.addAddress(InetAddress.getByName("debby"));
         long now = new Date().getTime() / 1000;
         peerGroup.setFastCatchupTimeSecs(now);
 
-        final long oneMonthAgo = now - (86400 * 30);
+        final long timeAgo = now - (86400 * options.valueOf(daysFlag));
+        System.out.println("Checkpointing up to " + Utils.dateTimeFormat(timeAgo * 1000));
 
-        chain.addListener(new AbstractBlockChainListener() {
+        chain.addNewBestBlockListener(Threading.SAME_THREAD, new NewBestBlockListener() {
             @Override
             public void notifyNewBestBlock(StoredBlock block) throws VerificationException {
                 int height = block.getHeight();
-                if ((height % PARAMS.getInterval() == 2015 || height % PARAMS.getInterval() == 0) && block.getHeader().getTimeSeconds() <= oneMonthAgo) {
-                    System.out.println(String.format("Checkpointing block %s at height %d",
-                            block.getHeader().getHash(), block.getHeight()));
+                if (height % params.getInterval() == 0 && block.getHeader().getTimeSeconds() <= timeAgo) {
+                    System.out.println(String.format("Checkpointing block %s at height %d, time %s",
+                            block.getHeader().getHash(), block.getHeight(), Utils.dateTimeFormat(block.getHeader().getTime())));
                     checkpoints.put(height, block);
                 }
             }
-        }, Threading.SAME_THREAD);
+        });
 
-        peerGroup.startAsync();
-        peerGroup.awaitRunning();
         peerGroup.downloadBlockChain();
 
         checkState(checkpoints.size() > 0);
 
-        // Write checkpoint data out.
-        writeBinaryCheckpoints(checkpoints, PLAIN_CHECKPOINTS_FILE);
-        writeTextualCheckpoints(checkpoints, TEXTUAL_CHECKPOINTS_FILE);
+        final File plainFile = new File("checkpoints" + suffix);
+        final File textFile = new File("checkpoints" + suffix + ".txt");
 
-        peerGroup.stopAsync();
-        peerGroup.awaitTerminated();
+        // Write checkpoint data out.
+        writeBinaryCheckpoints(checkpoints, plainFile);
+        writeTextualCheckpoints(checkpoints, textFile);
+
+        peerGroup.stop();
         store.close();
 
         // Sanity check the created files.
-        sanityCheck(PLAIN_CHECKPOINTS_FILE, checkpoints.size());
-        sanityCheck(TEXTUAL_CHECKPOINTS_FILE, checkpoints.size());
+        sanityCheck(plainFile, checkpoints.size());
+        sanityCheck(textFile, checkpoints.size());
     }
 
     private static void writeBinaryCheckpoints(TreeMap<Integer, StoredBlock> checkpoints, File file) throws Exception {
         final FileOutputStream fileOutputStream = new FileOutputStream(file, false);
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        MessageDigest digest = Sha256Hash.newDigest();
         final DigestOutputStream digestOutputStream = new DigestOutputStream(fileOutputStream, digest);
         digestOutputStream.on(false);
         final DataOutputStream dataOutputStream = new DataOutputStream(digestOutputStream);
@@ -118,7 +188,7 @@ public class BuildCheckpoints {
             buffer.position(0);
         }
         dataOutputStream.close();
-        Sha256Hash checkpointsHash = new Sha256Hash(digest.digest());
+        Sha256Hash checkpointsHash = Sha256Hash.wrap(digest.digest());
         System.out.println("Hash of checkpoints data is " + checkpointsHash);
         digestOutputStream.close();
         fileOutputStream.close();
@@ -141,19 +211,33 @@ public class BuildCheckpoints {
     }
 
     private static void sanityCheck(File file, int expectedSize) throws IOException {
-        CheckpointManager manager = new CheckpointManager(PARAMS, new FileInputStream(file));
+        FileInputStream fis = new FileInputStream(file);
+        CheckpointManager manager;
+        try {
+            manager = new CheckpointManager(params, fis);
+        } finally {
+            fis.close();
+        }
+
         checkState(manager.numCheckpoints() == expectedSize);
 
-        if (PARAMS.getId().equals(NetworkParameters.ID_MAINNET)) {
+        if (params.getId().equals(NetworkParameters.ID_MAINNET)) {
             StoredBlock test = manager.getCheckpointBefore(1390500000); // Thu Jan 23 19:00:00 CET 2014
             checkState(test.getHeight() == 280224);
             checkState(test.getHeader().getHashAsString()
                     .equals("00000000000000000b5d59a15f831e1c45cb688a4db6b0a60054d49a9997fa34"));
-        } else if (PARAMS.getId().equals(NetworkParameters.ID_TESTNET)) {
+        } else if (params.getId().equals(NetworkParameters.ID_TESTNET)) {
             StoredBlock test = manager.getCheckpointBefore(1390500000); // Thu Jan 23 19:00:00 CET 2014
             checkState(test.getHeight() == 167328);
             checkState(test.getHeader().getHashAsString()
                     .equals("0000000000035ae7d5025c2538067fe7adb1cf5d5d9c31b024137d9090ed13a9"));
         }
+    }
+
+    private static void startPeerGroup(PeerGroup peerGroup, InetAddress ipAddress) {
+        final PeerAddress peerAddress = new PeerAddress(params, ipAddress);
+        System.out.println("Connecting to " + peerAddress + "...");
+        peerGroup.addAddress(peerAddress);
+        peerGroup.start();
     }
 }
